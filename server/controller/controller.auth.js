70583fda9660328ca108d17b5eeb6420948e1163
FernandesTato <fernandesbarbosatato@gmail.com>
2025-10-04 20:41:07 -0300

    modifications on auth

--- server/controller/controller.auth.js
+++ server/controller/controller.auth.js
@@ -4,21 +4,22 @@ const bcrypt = require("bcrypt")
 
 const {groupModel, userModel} = require("../db/db.schema.js")
 
-const postSignup = async (req, res) => {
+const postUser = async (req, res) => {
 	try{
     const { username, pwd } = req.body
 
     if(!username || !pwd){
-      res.status(500).json({ error:"username or password don't have been succesfully sended"})
+      return res.status(500).json({ error:"username or password don't have been succesfully sended"})
     }
 
-		const salt = await bcrypt.genSalt(3) //genSalt needs to be 10 or 12(my hardware is no a big thing)
-		const cryptedPwd = await bcrypt.hash(pwd, salt)
+		const salt = await bcrypt.genSalt(3)
+    const cryptedPwd = await bcrypt.hash(pwd, salt)
 
 		const userDoc = new userModel({
 			name: username,
 			password: cryptedPwd
 		})
+
 		await userDoc.save()
 	} catch(err){
 		console.log(err);
@@ -28,10 +29,7 @@ const postSignup = async (req, res) => {
 	res.status(200).json({ message:"message"})
 }
 
-const getUser = (req, res) => {
-}
-
-const creatGroup = async (req, res) => {
+const postGroup = async (req, res) => {
   try{
     const {groupName, groupPwd} = req.body.group;
 
@@ -39,8 +37,8 @@ const creatGroup = async (req, res) => {
       return res.status(500).json({ mensage:"Envie um group certo" })
     } 
 
-		const salt = await bcrypt.genSalt(3) //genSalt needs to be 10 or 12(my hardware is not a big thing)
-		const cryptedPwd = await bcrypt.hash(groupPwd, salt)
+		const salt = await bcrypt.genSalt(3)
+    const cryptedPwd = await bcrypt.hash(groupPwd, salt)
 
     const groupDoc = userModel({
       name: groupName,
