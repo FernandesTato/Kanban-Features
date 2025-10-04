07e86337e0ddc8c3374cd6fa1d94fb27c0e00085
FernandesTato <fernandesbarbosatato@gmail.com>
2025-10-04 03:28:47 -0300

    changes at the name of group variables

--- server/controller/controller.auth.js
+++ server/controller/controller.auth.js
@@ -1,30 +1,27 @@
+require("dotenv").config()
 const jwt = require("jsonwebtoken")
 const bcrypt = require("bcrypt")
 
 const {groupModel, userModel} = require("../db/db.schema.js")
 
-require("dotenv").config()
-
 const postSignup = async (req, res) => {
-	const username = req.body.username
-	const pwd = req.body.pwd
-
-	if(!username || !pwd){
-		res.status(500).json({ error:"username or password don't have been succesfully sended"})
-	}
-
 	try{
+    const { username, pwd } = req.body
+
+    if(!username || !pwd){
+      res.status(500).json({ error:"username or password don't have been succesfully sended"})
+    }
+
 		const salt = await bcrypt.genSalt(3) //genSalt needs to be 10 or 12(my hardware is no a big thing)
 		const cryptedPwd = await bcrypt.hash(pwd, salt)
+
 		const userDoc = new userModel({
 			name: username,
 			password: cryptedPwd
 		})
-
 		await userDoc.save()
 	} catch(err){
 		console.log(err);
-
 		return res.status(500).json({ error:"cant create User"})
 	}
 
@@ -35,22 +32,22 @@ const getUser = (req, res) => {
 }
 
 const creatGroup = async (req, res) => {
-  const [nameGroup, pwdGroup] = req.body.group;
-
-  if(!pwdGroup || pwdGroup != String || !nameGroup){
-    return res.status(500).json({ mensage:"Envie um group certo" })
-  } 
-
   try{
+    const {groupName, groupPwd} = req.body.group;
+
+    if(!groupPwd ||groupPwd != String || !groupName){
+      return res.status(500).json({ mensage:"Envie um group certo" })
+    } 
+
 		const salt = await bcrypt.genSalt(3) //genSalt needs to be 10 or 12(my hardware is not a big thing)
-		const cryptedPwd = await bcrypt.hash(pwdGroup, salt)
+		const cryptedPwd = await bcrypt.hash(groupPwd, salt)
 
     const groupDoc = userModel({
-      name: nameGroup,
+      name: groupName,
       password: cryptedPwd, 
     })
 
-    await grouopDoc.save()
+    await groupDoc.save()
   } catch (err){
     return res.status(400).json({ error:`Some error happens: ${err}`})
   }
