635573fd4faf3a468c4cf7522e95efb1b86fa89a
FernandesTato <fernandesbarbosatato@gmail.com>
2025-10-04 03:20:09 -0300

    rename variables and aligned the schema with the model

--- server/controller/controller.auth.js
+++ server/controller/controller.auth.js
@@ -1,31 +1,33 @@
 const jwt = require("jsonwebtoken")
-const userModel = require("../db/db.js")
 const bcrypt = require("bcrypt")
-const groupModel = require("../db/db.js")
+
+const {groupModel, userModel} = require("../db/db.schema.js")
 
 require("dotenv").config()
 
 const postSignup = async (req, res) => {
 	const username = req.body.username
 	const pwd = req.body.pwd
-	const userId = req.body.userId
 
 	if(!username || !pwd){
 		res.status(500).json({ error:"username or password don't have been succesfully sended"})
 	}
+
 	try{
 		const salt = await bcrypt.genSalt(3) //genSalt needs to be 10 or 12(my hardware is no a big thing)
 		const cryptedPwd = await bcrypt.hash(pwd, salt)
-		const userObj = new userModel({
+		const userDoc = new userModel({
 			name: username,
-			password: cryptedPwd,
-			userId: userId
+			password: cryptedPwd
 		})
-		await userObj.save()
+
+		await userDoc.save()
 	} catch(err){
 		console.log(err);
+
 		return res.status(500).json({ error:"cant create User"})
 	}
+
 	res.status(200).json({ message:"message"})
 }
 
@@ -34,21 +36,21 @@ const getUser = (req, res) => {
 
 const creatGroup = async (req, res) => {
   const [nameGroup, pwdGroup] = req.body.group;
-  const admingId = req.body.admingId;
 
   if(!pwdGroup || pwdGroup != String || !nameGroup){
     return res.status(500).json({ mensage:"Envie um group certo" })
   } 
+
   try{
 		const salt = await bcrypt.genSalt(3) //genSalt needs to be 10 or 12(my hardware is not a big thing)
 		const cryptedPwd = await bcrypt.hash(pwdGroup, salt)
 
-    const groupObj = userModel({
+    const groupDoc = userModel({
       name: nameGroup,
       password: cryptedPwd, 
-      admingId: admingId
     })
-    await grouopObj.save()
+
+    await grouopDoc.save()
   } catch (err){
     return res.status(400).json({ error:`Some error happens: ${err}`})
   }
