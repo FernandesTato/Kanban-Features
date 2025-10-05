432219477d6a89ffa5886b3b9649590a36a89157
FernandesTato <fernandesbarbosatato@gmail.com>
2025-10-05 20:21:55 -0300

    Authentication with jwt

--- server/controller/controller.auth.js
+++ server/controller/controller.auth.js
@@ -1,54 +1,60 @@
 require("dotenv").config()
 const jwt = require("jsonwebtoken")
 const bcrypt = require("bcrypt")
+const { groupModel, userModel } = require("../db/db.schema.js")
 
-const {groupModel, userModel} = require("../db/db.schema.js")
+const maxAge = 1 //just a random number for while
 
-const postUser = async (req, res) => {
-	try{
-    const { username, pwd } = req.body
-
-    if(!username || !pwd){
-      return res.status(500).json({ error:"username or password don't have been succesfully sended"})
-    }
-
-		const salt = await bcrypt.genSalt(3)
-    const cryptedPwd = await bcrypt.hash(pwd, salt)
-
-		const userDoc = new userModel({
-			name: username,
-			password: cryptedPwd
-		})
-
-		await userDoc.save()
-	} catch(err){
-		console.log(err);
-		return res.status(500).json({ error:"cant create User"})
-	}
-
-	res.status(200).json({ message:"message"})
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.SECRE_KEY, {
+    experesIn:maxAge
+  })
+}
+const encrypting = async (pwd) => {
+  const salt = await bcrypt.genSalt(3)
+  const cryptedPwd = await bcrypt.hash(pwd, salt)
+  return cryptedPwd
+}
+const saveOnDb = async (username, pwd, model) => {
+  if()
+  const cryptedPwd = await encrypting(pwd)
+  const doc = new model({
+    name: username,
+    password: cryptedPwd
+  })
+  doc.save()
+  return doc
 }
 
-const postGroup = async (req, res) => {
+const userSignup = async (req, res) => {
+  const { username, pwd } = req.body
+	try{
+    if(!username || !pwd){
+      throw new Error("error on username or password")
+    }
+
+    const user = await saveOnDb(username, pwd, userModel)
+    const jwtToken = createToken{user._id}
+
+    res.cookie('jwt', token, { httpOnly: true, maxAge:maxAge})
+    res.status(201).json({ user: user._id })
+	} catch(err){
+    console.error(`Error: ${err}`) //It is needed to make some methods on db schema to throw error which we can read and laed with it in a handle error function
+    res.status(400).json({ error: err})
+	}
+	res.status(200).json({ message:"sigup was completed succesfully"})
+}
+
+const groupSignup = async (req, res) => {
+  const { groupName, pwd } = req.body
   try{
-    const {groupName, groupPwd} = req.body.group;
-
-    if(!groupPwd ||groupPwd != String || !groupName){
-      return res.status(500).json({ mensage:"Envie um group certo" })
-    } 
-
-		const salt = await bcrypt.genSalt(3)
-    const cryptedPwd = await bcrypt.hash(groupPwd, salt)
-
-    const groupDoc = userModel({
-      name: groupName,
-      password: cryptedPwd, 
-    })
-
-    await groupDoc.save()
-  } catch (err){
-    return res.status(400).json({ error:`Some error happens: ${err}`})
+    if(!groupName || !pwd){
+      throw new Error("name or password is invalid")
+    }
+    const group = await saveOnDb(groupName, pwd, groupModel)
+  } catch(err) {
+    res.status(400).json({ error: err})
   }
 }
 
-module.exports = { postSignup, getUser }
+module.exports = { userSignup, groupSignup }
--- server/router/router.js
+++ server/router/router.js
@@ -1,10 +1,9 @@
 const express = require("express")
 const { postCards } = require("../controller/controller.board.js")
+const { userSignup, groupSignup } = require("../controller/controller.auth.js")
 const router = express.Router()
 
-router.post("/board", postCards)
+router.post("/login/user", userSignup)
+router.post("/login/group", groupSignup)
 
 module.exports = router
-
-
-
