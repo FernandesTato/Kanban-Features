c21fa64fc4b0c722cc48a10c355f74f5ae51f172
FernandesTato <fernandesbarbosatato@gmail.com>
2025-10-05 20:37:14 -0300

    I will restart the board, I didn't like the way that code was taking

--- server/controller/controller.board.js
+++ server/controller/controller.board.js
@@ -1,27 +1,2 @@
 const { groupModel,  userModel, carModel } = require("../db/schema.js")
-
-const postCards = async (req, res) => {
-  try{
-    const cardsArrays = req.body.cards;
-
-    if(!Array.isArray(cardsArrays) || cardsArrays.length === 0){
-      return res.status(400).json({ error: "lista de cards vazia"})
-    }
-
-    res.status(201).json({ message: "cards created"})
-  } catch(error){
-    res.status(500).json({ error: error.message})
-  }
-}
-
-const getCards = async (req, res) => {
- try{
-    const cards = await cardModel.find()
-    res.status(200).json({ card: cards })
-  } catch(err){
-    console.error(err)
-    return res.status(500).json({ message: err.message })
-  }
-}
-
 module.exports = { postCards, getCards }
