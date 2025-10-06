412ab66d3180ade1580b314be03eb922231d4aee
FernandesTato <fernandesbarbosatato@gmail.com>
2025-10-06 16:50:06 -0300

    fixing syntax at the db connection

--- server/db/db.js
+++ server/db/db.js
@@ -4,13 +4,13 @@ require("dotenv").config()
 const uri = process.env.DB_URI
 
 const connectingToMongoDb = async () => {
-    try {
-        await mongoose.connect(uri)
-        console.log("connected to db")
-        console.log(uri)
-    } catch(err) {
-        return console.log(`error ${err}`)
-    }
+  try {
+      await mongoose.connect(uri)
+      console.log("connected to db")
+      console.log(uri)
+  } catch(err) {
+      return console.log(`error ${err}`)
+  }
 }
 
 
